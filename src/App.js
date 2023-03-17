fa7f9afcc0d90516752ac58bb3dd25e665cd8943
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-03-17 22:20:08 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React react_test
+          Learn React 
         </a>
       </header>
     </div>
