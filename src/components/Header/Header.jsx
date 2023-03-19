903fd0e915b56ba5a9394b19b35084039f310ee2
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-03-19 20:26:12 +0400

    first logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./Header.scss";
 import { Container } from "./../../layouts/Container";
+import { ReactComponent as LogoIcon } from "./../../assets/img/logo.svg";
 
 
 
 function Header() {
     return (
         <Container>
-            <div className='header flex justify-between'>
-                <p>hello world</p>
-                <p>menu</p>
+           <div className='flex justify-between py-2 mb-4'>
+                <div className='Logo'>
+                    <LogoIcon className="mr-2" style={{width:'100%'}}/>
+                    <span>React template </span>
+                </div>
             </div>
         </Container>
       );
