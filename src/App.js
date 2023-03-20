2e864b1665ea77e3b889afa62ebb0b0370b9e1a4
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-03-20 14:20:16 +0400

    validating props

--- src/App.js
+++ src/App.js
@@ -10,10 +10,41 @@ import  "./test.js";
 function App() {
   return (
     <div className="ui-wrapper">
-      <Header />
+      <Header isLogo isFixed />
       <div className="ui-content-wrapper">
         <Container>
-          <p className="text-2xl">template</p>
+          <p className="text-2xl">
+          Home
+          Scup medaka snake mudhead sea bream damselfish tui chub, masu salmon, toadfish warbonnet pumpkinseed shiner sea catfish wasp fish shortnose chimaera. Collared dogfish flier largemouth bass garden eel. Bull shark tetra sábalo, Rainbowfish, marlin yellowfin croaker pikeblenny kuhli loach.<br></br>
+          <br></br>
+          Gila trout brook lamprey bull shark central mudminnow. Bass flying characin climbing gourami bass walu banjo catfish ilisha African lungfish.
+          <br></br>
+          <br></br>
+          Inanga buri, albacore weasel shark swamp-eel wahoo chubsucker kelpfish Black angelfish. Galjoen fish lenok pike eel longnose sucker Ragfish arapaima ponyfish trumpetfish.
+          <br></br>
+          <br></br>
+          Climbing catfish cornetfish Rio Grande perch zebra bullhead shark California halibut, skipjack tuna oarfish velvet-belly shark. Grenadier char mackerel. Black mackerel, ayu sand tilefish spikefish, Dolly Varden trout, boga boafish freshwater hatchetfish Pacific lamprey zingel fangtooth. Mudfish grunter boga, sheepshead minnow pejerrey codlet. Whitefish spiny eel rudd European flounder glassfish gulf menhaden neon tetra. Vendace greenling Spanish mackerel channel catfish wolffish largemouth bass, bullhead.
+          <br></br>
+          <br></br>
+
+          White shark amur pike warbonnet cat shark giant wels electric eel gouramie. Queen danio angelfish barbel blue eye x-ray tetra longnose dace, Molly Miller sand stargazer African glass catfish. Alaska blackfish angelfish: flyingfish, burbot mako shark ballan wrasse, goby southern hake; triggerfish zebra trout arapaima, sixgill ray." Emperor bream angelfish, knifefish spadefish stickleback, whale shark Mozambique tilapia, sardine hillstream loach pike eel tubeblenny.
+          <br></br>
+          <br></br>
+
+          Surgeonfish x-ray tetra wasp fish lefteye flounder batfish shortnose chimaera snailfish scythe butterfish sleeper shark ray mackerel, "Asian carps jackfish loweye catfish." Sergeant major prickly shark collared carpetshark ghost flathead: smooth dogfish yellow-edged moray elasmobranch, three-toothed puffer; zebra danio! Black swallower, dogfish; channel bass central mudminnow, Antarctic icefish loach goby. Wolffish whiting searobin codling, cuckoo wrasse, loach crevice kelpfish coley jewfish amago porbeagle shark.
+          <br></br>
+          <br></br>
+
+          Walking catfish Indian mul cardinalfish, carpetshark remora. Jack icefish stonecat lake trout--glass catfish cuskfish flying characin, shrimpfish roanoke bass."
+          <br></br>
+          <br></br>
+
+          Sea toad dragonet longfin escolar, temperate bass cepalin paradise fish yellowfin tuna, collared carpetshark gulf menhaden turbot warty angler. Emperor weeverfish Black triggerfish silver hake Australian lungfish jewfish, sturgeon cepalin anchovy beaked salmon king of herring. Glass catfish lighthousefish, "greenling murray cod cookie-cutter shark boga quillback stickleback loach minnow marlin sea snail." Mud minnow: thorny catfish burrowing goby waryfish yellowtail kingfish spiny-back pufferfish striped bass rough scad. Common carp yellow moray sauger warty angler; northern clingfish triggerfish Razorback sucker. Dragon goby, green swordtail; Mexican blind cavefish Black tetra, "southern flounder danio firefish; threadfin." Rock cod tube-snout pomfret filefish pike sandfish loach minnow gizzard shad swallower kokanee, blue triggerfish glass catfish, pencilfish zebra lionfish. Mosquitofish rock bass sailbearer glassfish, taimen yellowmargin triggerfish spiny-back ribbon sawtail fish.
+          <br></br>
+          <br></br>
+
+          Amur pike swamp-eel sea raven Antarctic cod pelican gulper, yellowtail kingfish beaked salmon staghorn sculpin. Beachsalmon scorpionfish barracudina tigerperch Old World knifefish, whiting Kafue pike, tadpole fish electric stargazer. Pompano dolphinfish, temperate perch cookie-cutter shark three-toothed puffer tonguefish southern Dolly Varden! Zebra trout shark pickerel luderick triplefin blenny salmon shark shrimpfish common carp snipe eel unicorn fish. Sea chub Sacramento blackfish rough pomfret fire bar danio tapetail Atlantic saury.
+          </p>
         </Container>
       </div>
       <Footer />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import "./Header.scss";
 import { Container } from "./../../layouts/Container";
 import { ReactComponent as LogoIcon } from "./../../assets/img/logo.svg";
+import propTypes from "prop-types"
 
 
 
-function Header() {
+function Header({isLogo, isFixed}) {
     return (
         <Container>
-           <div className='flex justify-between py-2 mb-4'>
+           <div className={`flex justify-between py-2 mb-4 ${isFixed && 'isFixed'}`}>
                 <div className='Logo'>
-                    <LogoIcon className="mr-2" style={{width:'100%'}}/>
-                    <span>React template </span>
+                    {isLogo && <LogoIcon />}
+                    <span>React app </span>
                 </div>
+                <ul className='ui-button-group'>
+                    <li className='ui-link'>Home</li>
+                    <li className='ui-link'>About</li>
+                </ul>
             </div>
         </Container>
       );
 }
+
+Header.propTypes = {
+    isLogo: propTypes.bool,
+    isFixed: propTypes.bool,
+};
+Header.defaultTypes = {
+    isLogo: false,
+    isFixed: true,
+};
 
 export default Header;
