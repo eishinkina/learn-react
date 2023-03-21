215b86e91cbdcb4761e2e8587c925c3bfa12e556
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-03-21 14:06:02 +0400

    className 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,22 @@ import { Container } from "./../../layouts/Container";
 
 function Footer () {
     return (
-        <Container>
-            <div className='footer'>
-                <p>footer</p>
-            </div>
-        </Container>
+        <Footer className="Footer">
+            <Container>
+                <div className="py-2 mt-6">
+                    <p className='text-center text-xs' style={{opacity: 0.6}}>
+                        сделано на курсе
+                        <a
+                        target="_blank"
+                        href='https://app.tocode.ru/courses/react-pro/react-app-1/406/'
+                        className='ui-link ml-1'
+                        >
+                            React.js - с нуля до результата!
+                        </a>
+                    </p>
+                </div>
+            </Container>
+        </Footer>
     );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,21 @@ import "./Header.scss";
 import { Container } from "./../../layouts/Container";
 import { ReactComponent as LogoIcon } from "./../../assets/img/logo.svg";
 import propTypes from "prop-types"
+import classNames from 'classnames';
 
 
 
-function Header({isLogo, isFixed}) {
+function Header({isLogo, isFixed, className}) {
+    const classes = classNames("Header",
+    className, 
+    {
+        isFixed: isFixed, 
+    }
+    ); 
     return (
-        <Container title='123' alt='asd' style={{display: 'block'}}>
-           <div className={`flex justify-between py-2 mb-4 ${isFixed && 'isFixed'}`}>
+        <header className={classes}>
+        <Container>
+           <div className=" flex justify-between py-2 mb-4">
                 <div className='Logo'>
                     {isLogo && <LogoIcon />}
                     <span>React app </span>
@@ -20,12 +28,14 @@ function Header({isLogo, isFixed}) {
                 </ul>
             </div>
         </Container>
+        </header>
       );
 }
 
 Header.propTypes = {
     isLogo: propTypes.bool,
     isFixed: propTypes.bool,
+    className: propTypes.string,
 };
 Header.defaultTypes = {
     isLogo: false,
